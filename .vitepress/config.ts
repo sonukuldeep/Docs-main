1d19bd4f7c9d8bfb26d0c7bffb3538a4fffaaf19
sonukuldeep <sonukuldeep@outlook.com>
2023-07-07 20:42:55 +0530

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Blogs', link: '/markdown-examples' }
+      { text: 'Blogs', link: '/intro-page' }
     ],
     search: { provider: 'local' },
     sidebar: [
